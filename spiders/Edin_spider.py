c0f0f2a68c5da86f69014279b92a80623ddcee39
Arash-kariznovi <kariznoviarash@gmail.com>
2021-06-02 22:57:54 +0430

    recognize method added

--- spiders/Edin_spider.py
+++ spiders/Edin_spider.py
@@ -5,28 +5,35 @@ class QuotesSpider(scrapy.Spider):
     name = "Edinburgh"
 
     def start_requests(self):
+
         urls = [
             'http://www.drps.ed.ac.uk/20-21/dpt/cxartx08053.htm',
-            'http://www.drps.ed.ac.uk/20-21/dpt/cxchem08022.htm'
+            'http://www.drps.ed.ac.uk/20-21/dpt/cxchem08022.htm',
+            'http://www.drps.ed.ac.uk/17-18/dpt/cxcast08004.htm',
+            'http://www.drps.ed.ac.uk/17-18/dpt/cxlaws10211.htm'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
+    def recognize(self,response,column):
+        webpage = response.xpath('.//tr/td/text()').extract()
+        if column in webpage:
+            return webpage[webpage.index(column)+1]
     def parse(self, response):
+
         yield {
             'University': 'Edinburgh',
-            'Department': response.css("td::text").re('College of.*')[0],
-            'Course title':'',
-            'Professor':'',
-            'Objective':'',
-            'Prerequisite':'',
-            'Required Skills':'',
-            'Outcome':'',
-            'References':'',
-            'Scores':'',
-            'Description':'',
+            'Department': self.recognize(response,'School'),
+            # 'Course title': self.recognize(response,'Pre-requisites'),
+            'Professor': self.recognize(response,'Course organiser'),
+            # 'Objective':'',
+            'Prerequisite':self.recognize(response,'Pre-requisites'),
+            # 'Required Skills':'',
+            # 'Outcome':'',
+            # 'References':'',
+            # 'Scores':response.xpath('//td/text()')[87].get(),
+            'Description':self.recognize(response,'Course description'),
             'University Homepage': "https://www.ed.ac.uk/",
-            'course Homepage':'',
+            # 'course Homepage':'',
             'Professor Email':''
-
         }
