24b6c2b6c11b8c717b5d3f2ee7f549e23d06e562
Arash-kariznovi <kariznoviarash@gmail.com>
2021-06-03 01:05:37 +0430

    extraction completed

--- spiders/Edin_spider.py
+++ spiders/Edin_spider.py
@@ -6,34 +6,52 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
 
+        # response.xpath('.//td/text()')[98].extract()
         urls = [
             'http://www.drps.ed.ac.uk/20-21/dpt/cxartx08053.htm',
             'http://www.drps.ed.ac.uk/20-21/dpt/cxchem08022.htm',
             'http://www.drps.ed.ac.uk/17-18/dpt/cxcast08004.htm',
-            'http://www.drps.ed.ac.uk/17-18/dpt/cxlaws10211.htm'
+            'http://www.drps.ed.ac.uk/17-18/dpt/cxlaws10211.htm',
+            'http://www.drps.ed.ac.uk/17-18/dpt/cxelee10006.htm',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
+
     def recognize(self,response,column):
+
         webpage = response.xpath('.//tr/td/text()').extract()
-        if column in webpage:
+        if column in webpage and  not column == "Email:":
             return webpage[webpage.index(column)+1]
+
+        else:
+            webpage = response.xpath('.//text()').extract()
+            if column in webpage:
+                if column=="Reading List":
+                    return webpage[webpage.index(column) + 2]
+                elif column == "Email:":
+                    return webpage[webpage.index(column) + 1]
+                elif column == "Learning Outcomes":
+                    temp = webpage[webpage.index(column) + 3]
+                    return temp
+
+
     def parse(self, response):
 
         yield {
             'University': 'Edinburgh',
             'Department': self.recognize(response,'School'),
-            # 'Course title': self.recognize(response,'Pre-requisites'),
+            'Course title': response.xpath('.//h1/text()')[1].getall(),
             'Professor': self.recognize(response,'Course organiser'),
-            # 'Objective':'',
+            'Objective':self.recognize(response,'Graduate Attributes and Skills'),
             'Prerequisite':self.recognize(response,'Pre-requisites'),
             # 'Required Skills':'',
-            # 'Outcome':'',
-            # 'References':'',
-            # 'Scores':response.xpath('//td/text()')[87].get(),
+            #some bugs below
+            'Outcome':self.recognize(response,'Learning Outcomes'),
+
+            'References':self.recognize(response,'Reading List'),
+            'Scores': self.recognize(response,'Assessment '),
             'Description':self.recognize(response,'Course description'),
             'University Homepage': "https://www.ed.ac.uk/",
-            # 'course Homepage':'',
-            'Professor Email':''
+            'Professor Email':self.recognize(response,'Email:')
         }
